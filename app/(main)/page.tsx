4f641f66dce181c454ddd908b19780153a39d960
ericnd <e.ricdo@outlook.com>
2025-02-19 05:18:51 +1100

    fixed navbar

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,10 +3,10 @@ import SpinningTetrahedron from "./../(components)/tetra";
 export default function Home() {
   return (
     <div className="relative w-screen flex flex-col">
-      {/* Sticky Navbar at Bottom Left with higher z-index */}
-      <div className="fixed bottom-4 left-4 text-white p-2 rounded-lg z-50">
+      {/* Sticky Navbar at Top Right with higher z-index */}
+      <div className="fixed top-4 right-4 text-white p-2 rounded-lg z-50">
         <nav>
-          <ul className="flex flex-col space-y-2 text-left">
+          <ul className="flex sm:flex-row sm:space-x-4 flex-col sm:space-y-0 space-y-2 text-right">
             <li><a href="#about" className="hover:underline focus:outline-none">About</a></li>
             <li><a href="#work" className="hover:underline focus:outline-none">Work</a></li>
             <li><a href="#contact" className="hover:underline focus:outline-none">Contact</a></li>
